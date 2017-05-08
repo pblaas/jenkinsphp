211b8d64d1a51d2d73d22178f5efb15bce4e8edb
pblaas <pblaas@it-ernity.nl>
2017-05-08 13:02:03 +0200

    update

--- src/index.php
+++ src/index.php
@@ -2,7 +2,7 @@
 echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
 echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
 echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
-echo "Jenkins pipelines - 8 may ";
+echo "Jenkins pipelines - 8 May 13:01 ";
 ?>
 
 
