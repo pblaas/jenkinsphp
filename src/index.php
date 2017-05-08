7866c6454920a312e66a1a6e8b6b4e6f65b182a8
pblaas <pblaas@it-ernity.nl>
2017-05-08 09:17:59 +0200

    source stuff

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<?php
+echo "FQDN: ". $_SERVER['SERVER_NAME']."<br />";
+echo "Container: ". $_SERVER['SERVER_ADDR']."<br />";
+echo "UniqueID: ". $_SERVER['UNIQUE_ID']."<br />";
+echo "Jenkins pipelines - xxxtest 4";
+?>
+
+
